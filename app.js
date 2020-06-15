99138e26811bb2802f9ad6a65636b38558aeaef6
mudassir <mudassirwaseem25@gmail.com>
2020-06-15 08:54:58 -0700

    commit complete

---
+++ app.js
@@ -0,0 +1,107 @@
+// chapt # 1 task # 1========//
+
+// alert ("hello world");
+
+// chapt # 1 task # 2========//
+
+
+// document.write("hello world");
+
+
+// chapt # 1 task # 3========//
+
+// alert("welcome to JS land");
+
+// ("happy coding");
+ 
+// chapt # 1 task # 4========//
+// var a = ("welcome to js land")
+// alert(a)
+
+// var b = ("happy coding")
+// alert(b)
+
+// chapt # 1 task # 5========//
+
+
+// var x =("hello i am using JS through my web browser console")
+// alert(x)
+// console.log(x)
+
+// chapt # 1 task # 6========//
+
+// alert("hello world")
+
+// chapt # 2 task # 1========//
+
+// var username;
+
+// // chapt # 2 task # 2========//
+// var myname = ("Mudassir Waseem");
+
+// // chapt # 2 task # 3========//
+
+// var message ;
+
+// var message = ("hello world")
+// alert(message)
+
+// // chapt # 2 task # 4========//
+// var name = ("jhone doe");
+// var age = ("15 years old");
+// var skills = ("certified mobile applicationdevelopement");
+
+// alert(name);
+// alert(age);
+// alert(skills);
+
+// // chapt # 2 task # 6========//
+
+
+// var email = ("maddy@gmail.com")
+// alert("my email address is " + email)
+
+
+// // chapt # 2 task # 7========//
+
+// var book = (" A smarter way to learn JavaScript.")
+// alert("i am trying to learn from the book" + book)
+
+// // chapt # 2 task # 8========//
+
+// var x  = ("yah i can write HTML content through Javascript")
+// document.write(x)
+
+// // chapt # 2 task # 9========//
+
+// SKIP
+
+// EEEEEENNNNNNDDDDDDDDD
+
+
+// // chapt # 3 task # 1========//
+
+// var age = ("15 years old");
+// alert(age)
+
+// // chapt # 3 task # 2========//
+
+
+// var x = (" you have visited this site 14 times")
+// alert(x)
+
+// // chapt # 3 task # 3========//
+// var birthyear = ("my birth year is 1990 <br> data type  my decleard variable ios number")
+
+// document.write(birthyear)
+
+
+// // chapt # 3 task # 4========//
+
+// var x = prompt("Visitor’s name");
+// var y = prompt("Product title");
+// var z = +prompt("quantity");
+
+// document.write(x + y + z)
+
+// EEEEEEEEEENNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDd
